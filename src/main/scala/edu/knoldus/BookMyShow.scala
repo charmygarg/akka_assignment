2feaa00a4e41ba02e18c44b74ee558cdd6478da8
ksimar <simarpreet@knoldus.in>
2017-03-21 10:26:19 +0530

    Akka Assignment

--- src/main/scala/edu/knoldus/BookMyShow.scala
+++ src/main/scala/edu/knoldus/BookMyShow.scala
@@ -13,7 +13,7 @@ class BookMyShow extends Actor {
   }
 
   def checkBooked = {
-    Thread.sleep(100)
+    //Thread.sleep(100)
     if(BookMyShow.status) {
       BookMyShow.status = false
       "Your seat has been booked!!"
@@ -31,16 +31,13 @@ object BookMyShow extends App {
   val list = List.range(1, 10)
   val system = ActorSystem("BookMyShow")
   val ref1 = system.actorOf(Props[BookMyShow])
-  val ref2 = system.actorOf(Props[BookMyShow])
-  val ref3 = system.actorOf(Props[BookMyShow])
-  val ref4 = system.actorOf(Props[BookMyShow])
 
   implicit val timeout = Timeout(1000 seconds)
 
   val person1 = ref1 ? list(4)
-  val person2 = ref2 ? list(4)
-  val person3 = ref3 ? list(4)
-  val person4 = ref4 ? list(4)
+  val person2 = ref1 ? list(4)
+  val person3 = ref1 ? list(4)
+  val person4 = ref1 ? list(4)
 
   person1 foreach println
   person2 foreach println
